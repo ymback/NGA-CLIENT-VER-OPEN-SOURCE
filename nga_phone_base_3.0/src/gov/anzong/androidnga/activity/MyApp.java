317087de0a50b66a4715f8ee8d2184e2fc267c08
ymback <ymback@sayyoulove.me>
2014-06-10 22:23:43 +0800

    显示客户端
    Signed-off-by: ymback <ymback@sayyoulove.me>

--- nga_phone_base_3.0/src/gov/anzong/androidnga/activity/MyApp.java
+++ nga_phone_base_3.0/src/gov/anzong/androidnga/activity/MyApp.java
@@ -40,7 +40,7 @@ import com.nostra13.universalimageloader.utils.StorageUtils;
 
 public class MyApp extends Application implements PerferenceConstant {
 	final private static String TAG = MyApp.class.getSimpleName();
-	public final static int version = 2017;
+	public final static int version = 2018;
 	private PhoneConfiguration config = null;
 	boolean newVersion = false;
 	static final String RECENT = "ื๎ฝüทรฮส";
