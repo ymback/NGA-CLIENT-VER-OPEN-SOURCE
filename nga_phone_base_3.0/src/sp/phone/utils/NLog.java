9cd0f3f757979ee9a51d0545b8460a37fe02ef04
elrond.liu <elrond.liu@gmail.com>
2017-09-06 16:40:23 +0800

    bug, 图标

--- nga_phone_base_3.0/src/sp/phone/utils/NLog.java
+++ nga_phone_base_3.0/src/sp/phone/utils/NLog.java
@@ -12,42 +12,42 @@ import gov.anzong.androidnga.BuildConfig;
 public class NLog {
     public static int v(String tag, String msg) {
         if (BuildConfig.DEBUG)
-            return NLog.v(tag, msg);
+            return Log.v(tag, msg);
         else
             return 0;
     }
 
     public static int d(String tag, String msg) {
         if (BuildConfig.DEBUG)
-            return NLog.d(tag, msg);
+            return Log.d(tag, msg);
         else
             return 0;
     }
 
     public static int i(String tag, String msg) {
         if (BuildConfig.DEBUG)
-            return NLog.i(tag, msg);
+            return Log.i(tag, msg);
         else
             return 0;
     }
 
     public static int w(String tag, String msg) {
         if (BuildConfig.DEBUG)
-            return NLog.w(tag, msg);
+            return Log.w(tag, msg);
         else
             return 0;
     }
 
     public static int e(String tag, String msg) {
         if (BuildConfig.DEBUG)
-            return NLog.e(tag, msg);
+            return Log.e(tag, msg);
         else
             return 0;
     }
 
     public static int e(String tag, String msg, Throwable throwable) {
         if (BuildConfig.DEBUG)
-            return NLog.e(tag, msg, throwable);
+            return Log.e(tag, msg, throwable);
         else
             return 0;
     }
