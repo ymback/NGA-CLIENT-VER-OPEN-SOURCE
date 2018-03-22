11ddc840fa96893665d222a4a610bbb627a037ba
Justwen <yyhshrrr@126.com>
2018-03-22 22:34:40 +0800

    删除一些无用的文件和图片
    删除一些无用的文件和图片

--- nga_phone_base_3.0/src/sp/phone/interfaces/OnMessageDetialLoadFinishedListener.java
+++
@@ -1,9 +0,0 @@
-package sp.phone.interfaces;
-
-import sp.phone.bean.MessageDetailInfo;
-
-public interface OnMessageDetialLoadFinishedListener {
-    //void finishLoad(RSSFeed feed);
-    void finishLoad(MessageDetailInfo result);
-
-}
