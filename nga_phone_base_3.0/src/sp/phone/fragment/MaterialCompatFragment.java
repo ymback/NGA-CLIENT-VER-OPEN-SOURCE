0221dbd1bd1ba7eb9c99a84f7c2664cf364271a3
Justwen <yyhshrrr@126.com>
2018-04-12 19:49:32 +0800

    代码整理
    代码整理

--- nga_phone_base_3.0/src/sp/phone/fragment/MaterialCompatFragment.java
+++ nga_phone_base_3.0/src/sp/phone/fragment/MaterialCompatFragment.java
@@ -14,20 +14,15 @@ import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 
 import gov.anzong.androidnga.R;
-import sp.phone.fragment.BaseFragment;
-import sp.phone.interfaces.PullToRefreshAttacherOwner;
 import sp.phone.common.PhoneConfiguration;
-import uk.co.senab.actionbarpulltorefresh.extras.actionbarcompat.PullToRefreshAttacher;
 
-public abstract class MaterialCompatFragment extends BaseFragment implements PullToRefreshAttacherOwner {
+public abstract class MaterialCompatFragment extends BaseFragment {
 
     protected PhoneConfiguration mConfiguration = PhoneConfiguration.getInstance();
 
     protected static final String TAG = "material";
 
     private FloatingActionButton mFab;
-
-    private PullToRefreshAttacher mPullToRefreshAttacher;
 
     private int mLayoutId = R.layout.fragment_material;
 
@@ -43,7 +38,7 @@ public abstract class MaterialCompatFragment extends BaseFragment implements Pul
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(mLayoutId,container,false);
+        View rootView = inflater.inflate(mLayoutId, container, false);
         ViewGroup realContainer = (ViewGroup) rootView.findViewById(R.id.container);
         mTabLayout = (TabLayout) rootView.findViewById(R.id.tabs);
         mSpinner = (Spinner) rootView.findViewById(R.id.spinner);
@@ -51,8 +46,8 @@ public abstract class MaterialCompatFragment extends BaseFragment implements Pul
         setSupportActionBar(rootView);
         initSpinner(rootView);
         initFabButton(rootView);
-        View view = onCreateContainerView(inflater, realContainer,savedInstanceState);
-        if (view != null){
+        View view = onCreateContainerView(inflater, realContainer, savedInstanceState);
+        if (view != null) {
             realContainer.addView(view);
         }
         return rootView;
@@ -63,50 +58,50 @@ public abstract class MaterialCompatFragment extends BaseFragment implements Pul
         super.onViewCreated(view, savedInstanceState);
     }
 
-    protected void setTabViewPager(ViewPager viewPager){
-        if (mTabLayout != null){
+    protected void setTabViewPager(ViewPager viewPager) {
+        if (mTabLayout != null) {
             mTabLayout.setupWithViewPager(viewPager);
             mTabLayout.setVisibility(View.VISIBLE);
         }
     }
 
-    protected void setFabOnClickListener(View.OnClickListener listener){
-        if (mFab != null){
+    protected void setFabOnClickListener(View.OnClickListener listener) {
+        if (mFab != null) {
             mFab.setOnClickListener(listener);
             mFab.setVisibility(View.VISIBLE);
         }
     }
 
-    protected void setSpinnerAdapter(SpinnerAdapter adapter,AdapterView.OnItemSelectedListener listener){
-        if (mSpinner != null){
+    protected void setSpinnerAdapter(SpinnerAdapter adapter, AdapterView.OnItemSelectedListener listener) {
+        if (mSpinner != null) {
             mSpinner.setAdapter(adapter);
             mSpinner.setOnItemSelectedListener(listener);
             mSpinner.setVisibility(View.VISIBLE);
         }
     }
 
-    protected FloatingActionButton getFloatingActionButton(){
+    protected FloatingActionButton getFloatingActionButton() {
         return mFab;
     }
 
-    protected Spinner getSpinner(){
+    protected Spinner getSpinner() {
         return mSpinner;
     }
 
-    protected TabLayout getTabLayout(){
+    protected TabLayout getTabLayout() {
         return mTabLayout;
     }
 
-    protected void setLayoutId(int layoutId){
+    protected void setLayoutId(int layoutId) {
         mLayoutId = layoutId;
     }
 
 
-    protected int getContainerId(){
+    protected int getContainerId() {
         return R.id.container;
     }
 
-    private void setSupportActionBar(View rootView){
+    private void setSupportActionBar(View rootView) {
         Toolbar toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
         if (mActivity.getSupportActionBar() == null && toolbar != null) {
             mActivity.setSupportActionBar(toolbar);
@@ -115,13 +110,13 @@ public abstract class MaterialCompatFragment extends BaseFragment implements Pul
         }
     }
 
-    private void initSpinner(View rootView){
+    private void initSpinner(View rootView) {
         Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner);
-        if (spinner == null){
+        if (spinner == null) {
             return;
         }
         SpinnerAdapter adapter = getSpinnerAdapter();
-        if (adapter == null){
+        if (adapter == null) {
             spinner.setVisibility(View.GONE);
         } else {
             spinner.setVisibility(View.VISIBLE);
@@ -129,7 +124,7 @@ public abstract class MaterialCompatFragment extends BaseFragment implements Pul
             spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                 @Override
                 public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                    onSpinnerItemSelected((Spinner) parent,position);
+                    onSpinnerItemSelected((Spinner) parent, position);
                 }
 
                 @Override
@@ -140,46 +135,35 @@ public abstract class MaterialCompatFragment extends BaseFragment implements Pul
         }
     }
 
-    private void initFabButton(View rootView){
+    private void initFabButton(View rootView) {
         mFab = (FloatingActionButton) rootView.findViewById(R.id.fab);
         if (mFab == null) {
             return;
         }
         View.OnClickListener listener = getFabClickListener();
-        if (listener == null){
-            mFab .setVisibility(View.GONE);
+        if (listener == null) {
+            mFab.setVisibility(View.GONE);
         } else {
-            mFab .setVisibility(View.VISIBLE);
-            mFab .setOnClickListener(listener);
+            mFab.setVisibility(View.VISIBLE);
+            mFab.setOnClickListener(listener);
         }
     }
 
 
-    protected View.OnClickListener getFabClickListener(){
+    protected View.OnClickListener getFabClickListener() {
         return null;
     }
 
 
-    protected void onSpinnerItemSelected(Spinner spinner,int position){
+    protected void onSpinnerItemSelected(Spinner spinner, int position) {
 
     }
 
-    protected SpinnerAdapter getSpinnerAdapter(){
+    protected SpinnerAdapter getSpinnerAdapter() {
         return null;
     }
 
-    @Override
-    public PullToRefreshAttacher getAttacher() {
-        if (mPullToRefreshAttacher == null){
-            PullToRefreshAttacher.Options options = new PullToRefreshAttacher.Options();
-            options.refreshScrollDistance = 0.3f;
-            options.refreshOnUp = true;
-            mPullToRefreshAttacher = PullToRefreshAttacher.get(mActivity, options);
-        }
-        return mPullToRefreshAttacher;
-    }
-
-    public View onCreateContainerView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+    public View onCreateContainerView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return null;
     }
 }
