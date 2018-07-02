ea1ba039489103ddbfcbba7c6e6e8b740c933817
Justwen <yyhshrrr@126.com>
2018-07-02 20:20:20 +0800

    准备发版
    准备发版

--- nga_phone_base_3.0/src/main/java/sp/phone/common/VersionUpgradeTips.java
+++ nga_phone_base_3.0/src/main/java/sp/phone/common/VersionUpgradeTips.java
@@ -67,4 +67,11 @@ public interface VersionUpgradeTips {
             + "新版UI有任何bug，建议或意见请私信[@Justwen][@Elrond]\n"
             + "客户端吐槽QQ群:172503242,欢迎加入捡肥皂\n";
 
+    String TIPS_2084 = "更新内容\n"
+            + "● 修复一些在较低android版本上发生的崩溃问题\n"
+            + "● 修复无法保存动图的问题\n"
+            + "● 修复短消息界面域名错误的问题\n"
+            + "新版UI有任何bug，建议或意见请私信[@Justwen][@Elrond]\n"
+            + "客户端吐槽QQ群:172503242,欢迎加入捡肥皂\n";
+
 }
--- nga_phone_base_3.0/src/main/java/sp/phone/fragment/dialog/VersionUpgradeDialogFragment.java
+++ nga_phone_base_3.0/src/main/java/sp/phone/fragment/dialog/VersionUpgradeDialogFragment.java
@@ -17,7 +17,7 @@ public class VersionUpgradeDialogFragment extends BaseDialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setPositiveButton(android.R.string.ok,null)
                 .setTitle(R.string.prompt)
-        .setMessage(VersionUpgradeTips.TIPS_2083);
+        .setMessage(VersionUpgradeTips.TIPS_2084);
         return builder.create();
     }
 }
