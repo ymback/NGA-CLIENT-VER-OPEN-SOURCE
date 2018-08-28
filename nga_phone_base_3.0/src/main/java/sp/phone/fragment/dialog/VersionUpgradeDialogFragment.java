5fe507047c5ffe5a128b20b99a9884a6927b36a7
Justwen <yyhshrrr@126.com>
2018-08-28 21:32:15 +0800

    发版

--- nga_phone_base_3.0/src/main/java/sp/phone/fragment/dialog/VersionUpgradeDialogFragment.java
+++ nga_phone_base_3.0/src/main/java/sp/phone/fragment/dialog/VersionUpgradeDialogFragment.java
@@ -17,7 +17,7 @@ public class VersionUpgradeDialogFragment extends BaseDialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setPositiveButton(android.R.string.ok,null)
                 .setTitle(R.string.prompt)
-        .setMessage(VersionUpgradeTips.TIPS_2086);
+        .setMessage(VersionUpgradeTips.TIPS_2087);
         return builder.create();
     }
 }
