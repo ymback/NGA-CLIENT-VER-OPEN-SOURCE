f2d4b94fe5a2c815087bc877c3de7b49f07a12f5
Justwen <yyhshrrr@126.com>
2018-09-12 19:28:27 +0800

    发版
    发版

--- nga_phone_base_3.0/src/main/java/sp/phone/common/VersionUpgradeTips.java
+++ nga_phone_base_3.0/src/main/java/sp/phone/common/VersionUpgradeTips.java
@@ -108,4 +108,9 @@ public interface VersionUpgradeTips {
             + "新版UI有任何bug，建议或意见请私信[@Justwen][@Elrond]\n"
             + "客户端吐槽QQ群:172503242,欢迎加入捡肥皂\n";
 
+    String TIPS_2090 = "更新内容\n"
+            + "● 修复域名不可用的问题\n"
+            + "新版UI有任何bug，建议或意见请私信[@Justwen][@Elrond]\n"
+            + "客户端吐槽QQ群:172503242,欢迎加入捡肥皂\n";
+
 }
