875969f9eead06b4bc4a9bf2c767c818f279f4f3
Justwen <yyhshrrr@126.com>
2018-12-27 10:06:30 +0800

    加载本地黑名单html
    加载本地黑名单html

--- nga_phone_base_3.0/src/main/java/sp/phone/mvp/model/convert/builder/HtmlBlackListBuilder.java
+++ nga_phone_base_3.0/src/main/java/sp/phone/mvp/model/convert/builder/HtmlBlackListBuilder.java
@@ -1,11 +1,35 @@
 package sp.phone.mvp.model.convert.builder;
 
+import android.content.res.AssetManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import sp.phone.common.ApplicationContextHolder;
+
 /**
  * Created by Justwen on 2018/8/28.
  */
 public class HtmlBlackListBuilder {
 
+    private static String sBlackListHtml;
+
     public static String build() {
+        if (sBlackListHtml == null) {
+            AssetManager assetManager = ApplicationContextHolder.getContext().getAssets();
+            try (InputStream is = assetManager.open("html/black_list.html")) {
+                int length = is.available();
+                byte[] buffer = new byte[length];
+                is.read(buffer);
+                sBlackListHtml = new String(buffer, "utf-8");
+            } catch (IOException e) {
+                sBlackListHtml = getBlackListHtml();
+            }
+        }
+        return sBlackListHtml;
+    }
+
+    private static String getBlackListHtml() {
         return "<HTML> <HEAD><META http-equiv=Content-Type content= \"text/html; charset=utf-8 \">"
                 + "<body "
                 + "'>"
@@ -13,4 +37,6 @@ public class HtmlBlackListBuilder {
                 + "屏蔽"
                 + "]</font>" + "</font></body>";
     }
+
+
 }
