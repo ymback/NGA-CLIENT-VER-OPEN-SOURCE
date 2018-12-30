28f946e43c0f66c3565f56ca8fed87a353af0a07
Justwen <yyhshrrr@126.com>
2018-12-30 23:14:35 +0800

    准备发版
    准备发版

--- nga_phone_base_3.0/src/main/java/sp/phone/common/VersionUpgradeTips.java
+++ nga_phone_base_3.0/src/main/java/sp/phone/common/VersionUpgradeTips.java
@@ -168,4 +168,10 @@ public interface VersionUpgradeTips {
             + "新版UI有任何bug，建议或意见请私信[@Justwen][@Elrond]\n"
             + "客户端吐槽QQ群:172503242,欢迎加入捡肥皂\n";
 
+    String TIPS_2100 = "更新内容\n"
+            + "● 上传图片不再进行压缩\n"
+            + "新版UI有任何bug，建议或意见请私信[@Justwen][@Elrond]\n"
+            + "客户端吐槽QQ群:172503242,欢迎加入捡肥皂\n";
+
+
 }
