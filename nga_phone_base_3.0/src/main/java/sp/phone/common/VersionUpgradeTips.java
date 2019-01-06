d768ffeffd8ca460322f7de6b28d681392b53b25
Justwen <yyhshrrr@126.com>
2019-01-06 15:02:32 +0800

    更新提示
    更新提示

--- nga_phone_base_3.0/src/main/java/sp/phone/common/VersionUpgradeTips.java
+++ nga_phone_base_3.0/src/main/java/sp/phone/common/VersionUpgradeTips.java
@@ -175,6 +175,7 @@ public interface VersionUpgradeTips {
 
     String TIPS_2101 = "更新内容\n"
             + "● 解决无法上传图片的问题\n"
+            + "● 解决android P无法显示外链图片的问题\n"
             + "● 解决子板块状态错误的问题\n"
             + "新版UI有任何bug，建议或意见请私信[@Justwen][@Elrond]\n"
             + "客户端吐槽QQ群:172503242,欢迎加入捡肥皂\n";
