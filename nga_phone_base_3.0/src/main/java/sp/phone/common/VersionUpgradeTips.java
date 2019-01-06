4523d5776382595cdf0494531ab4f4d82c5b3d39
Justwen <yyhshrrr@126.com>
2019-01-06 14:06:41 +0800

    发版
    发版

--- nga_phone_base_3.0/src/main/java/sp/phone/common/VersionUpgradeTips.java
+++ nga_phone_base_3.0/src/main/java/sp/phone/common/VersionUpgradeTips.java
@@ -173,5 +173,11 @@ public interface VersionUpgradeTips {
             + "新版UI有任何bug，建议或意见请私信[@Justwen][@Elrond]\n"
             + "客户端吐槽QQ群:172503242,欢迎加入捡肥皂\n";
 
+    String TIPS_2101 = "更新内容\n"
+            + "● 解决无法上传图片的问题\n"
+            + "● 解决子板块状态错误的问题\n"
+            + "新版UI有任何bug，建议或意见请私信[@Justwen][@Elrond]\n"
+            + "客户端吐槽QQ群:172503242,欢迎加入捡肥皂\n";
+
 
 }
