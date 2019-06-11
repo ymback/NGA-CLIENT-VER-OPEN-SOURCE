d6ef360fe87b27028678ab5d52292f1ff32c37dc
Justwen <yyhshrrr@126.com>
2019-06-11 13:54:32 +0800

    更新默认值

--- nga_phone_base_3.0/src/main/java/sp/phone/common/Constants.java
+++ nga_phone_base_3.0/src/main/java/sp/phone/common/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 
     public static final int DEFAULT_TEXT_SIZE = 19;
 
-    public static final int TOPIC_TITLE_SIZE_DEFAULT = 14;
+    public static final int TOPIC_TITLE_SIZE_DEFAULT = 18;
     public static final int TOPIC_TITLE_SIZE_MIN = 1;
     public static final int TOPIC_TITLE_SIZE_MAX = 25;
 
@@ -19,7 +19,7 @@ public class Constants {
     public static final int EMOTICON_SIZE_MIN = 1;
     public static final int EMOTICON_SIZE_MAX = 200;
 
-    public static final int TOPIC_CONTENT_SIZE_DEFAULT = 14;
+    public static final int TOPIC_CONTENT_SIZE_DEFAULT = 18;
     public static final int TOPIC_CONTENT_SIZE_MIN = 1;
     public static final int TOPIC_CONTENT_SIZE_MAX = 34;
 }
