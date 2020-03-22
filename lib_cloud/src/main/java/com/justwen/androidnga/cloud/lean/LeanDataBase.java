eb6041ecb61cf41fcedf1613b2bcfd76c830bbdc
Justwen <yyhshrrr@126.com>
2020-03-22 11:27:30 +0800

    移除冗余的请求

--- lib_cloud/src/main/java/com/justwen/androidnga/cloud/lean/LeanDataBase.java
+++ lib_cloud/src/main/java/com/justwen/androidnga/cloud/lean/LeanDataBase.java
@@ -8,17 +8,9 @@ import com.justwen.androidnga.cloud.R;
 import com.justwen.androidnga.cloud.base.ICloudDataBase;
 import com.justwen.androidnga.cloud.base.VersionBean;
 
-import java.util.List;
-
 import cn.leancloud.AVLogger;
 import cn.leancloud.AVOSCloud;
 import cn.leancloud.AVObject;
-import cn.leancloud.AVQuery;
-import gov.anzong.androidnga.base.util.PreferenceUtils;
-import gov.anzong.androidnga.base.util.ToastUtils;
-import gov.anzong.androidnga.common.PreferenceKey;
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
 
 public class LeanDataBase implements ICloudDataBase {
 
@@ -37,104 +29,50 @@ public class LeanDataBase implements ICloudDataBase {
     }
 
     private void uploadAndroidVersionInfo() {
-        AVQuery<AVObject> query = new AVQuery<>("androidVersion");
-        query.whereEqualTo("versionName", mVersionBean.versionName);
-        query.whereEqualTo("androidVersion", Build.VERSION.SDK_INT);
-        query.findInBackground().subscribe(new Observer<List<AVObject>>() {
-            @Override
-            public void onSubscribe(Disposable disposable) {
-            }
-
-            @Override
-            public void onNext(List<AVObject> objects) {
-                if (objects != null && !objects.isEmpty()) {
-                    AVObject obj = objects.get(0);
-                    obj.increment("count");
-                    obj.saveInBackground().subscribe(new EmptyObserver<>());
-                } else {
-                    AVObject obj = new AVObject("androidVersion");
-                    obj.put("versionName", mVersionBean.versionName);
-                    obj.put("androidVersion", Build.VERSION.SDK_INT);
-                    obj.put("count", 1);
-                    obj.saveInBackground().subscribe(new EmptyObserver<>());
-                }
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        });
+        AVObject obj = new AVObject("androidVersion");
+        obj.put("versionName", mVersionBean.versionName);
+        obj.put("androidVersion", Build.VERSION.SDK_INT);
+        obj.increment("count");
+        obj.saveInBackground().subscribe(new EmptyObserver<>());
     }
+
 
     @Override
     public void uploadVersionInfo() {
-        AVQuery<AVObject> query = new AVQuery<>("version");
-        query.whereEqualTo("versionName", mVersionBean.versionName);
-        query.findInBackground().subscribe(new Observer<List<AVObject>>() {
-            @Override
-            public void onSubscribe(Disposable disposable) {
-            }
-
-            @Override
-            public void onNext(List<AVObject> objects) {
-                if (objects != null && !objects.isEmpty()) {
-                    AVObject obj = objects.get(0);
-                    obj.increment("count");
-                    obj.saveInBackground().subscribe(new EmptyObserver<>());
-                } else {
-                    AVObject obj = new AVObject("version");
-                    obj.put("versionName", mVersionBean.versionName);
-                    obj.put("count", 1);
-                    obj.saveInBackground().subscribe(new EmptyObserver<>());
-                }
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        });
+//        AVObject obj = new AVObject("version");
+//        obj.put("versionName", mVersionBean.versionName);
+//        obj.increment("count");
+//        obj.saveInBackground().subscribe(new EmptyObserver<>());
         uploadAndroidVersionInfo();
     }
 
     @Override
     public void checkUpgrade() {
-        AVQuery<AVObject> query = new AVQuery<>("versionUpgrade");
-        query.whereGreaterThan("versionCode", mVersionBean.versionCode);
-        query.findInBackground().subscribe(new Observer<List<AVObject>>() {
-            @Override
-            public void onSubscribe(Disposable disposable) {
-            }
-
-            @Override
-            public void onNext(List<AVObject> objects) {
-                if (objects != null && !objects.isEmpty()) {
-                    PreferenceUtils.putData(PreferenceKey.KEY_CHECK_UPGRADE_STATE, false);
-                    ToastUtils.show("新版本已发布， 关于界面可以查看最新版本！");
-                }
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        });
+//        AVQuery<AVObject> query = new AVQuery<>("versionUpgrade");
+//        query.whereGreaterThan("versionCode", mVersionBean.versionCode);
+//        query.findInBackground().subscribe(new Observer<List<AVObject>>() {
+//            @Override
+//            public void onSubscribe(Disposable disposable) {
+//            }
+//
+//            @Override
+//            public void onNext(List<AVObject> objects) {
+//                if (objects != null && !objects.isEmpty()) {
+//                    PreferenceUtils.putData(PreferenceKey.KEY_CHECK_UPGRADE_STATE, false);
+//                    ToastUtils.show("新版本已发布， 关于界面可以查看最新版本！");
+//                }
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//
+//            }
+//        });
     }
 
 
