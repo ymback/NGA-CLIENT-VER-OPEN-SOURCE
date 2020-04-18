8ed47e2ee4b0a3d5a14b1c57eb00b36ee774e516
Justwen <yyhshrrr@126.com>
2020-04-19 00:08:37 +0800

    忽略 FinalizerWatchdogDaemon 异常

--- lib_cloud/src/main/java/com/justwen/androidnga/cloud/bugly/BuglyWrapper.java
+++ lib_cloud/src/main/java/com/justwen/androidnga/cloud/bugly/BuglyWrapper.java
@@ -12,6 +12,7 @@ public class BuglyWrapper {
             int id = context.getResources().getIdentifier("bugly_app_id", "string", context.getOpPackageName());
             if (id > 0) {
                 CrashReport.initCrashReport(context, context.getString(id), false);
+                Thread.setDefaultUncaughtExceptionHandler(new BuglyExceptionHandlerProxy(Thread.getDefaultUncaughtExceptionHandler()));
             }
         }
     }
